92ead0e3526b9ce1084eb8af9601601a64309721
Felipe Bis <felipe.bis@aluno.ifsp.edu.br>
2023-02-26 13:29:10 -0300

    chore: adjust ts error

--- src/components/Temporizador.tsx
+++ src/components/Temporizador.tsx
@@ -16,11 +16,7 @@ export default function Temporizador(props: TemporizadorProps) {
                 size={120}
                 isPlaying
                 onComplete={props.tempoEsgotado}
-                colors={[
-                    ['#BCE596', 0.33],
-                    ['#F7B801', 0.33],
-                    ['#ED827A', 0.33],
-                ]}>
+                colors={['#BCE596', '#F7B801', '#ED827A']}>
                 {({ remainingTime }) => remainingTime}
             </CountdownCircleTimer>
         </div>
